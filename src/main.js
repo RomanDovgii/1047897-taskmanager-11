d635725f977cbb5d383e365bdd3988a7d52ee223
RomanDovgii <romandovgiiwork@gmail.com>
2020-04-23 18:32:27 +0300

    renderTask is a part of Task class from now on

--- src/main.js
+++ src/main.js
@@ -16,43 +16,6 @@ const TASK_COUNT = 22;
 const SHOWING_TASKS_COUNT_ON_START = 8;
 const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
 
-// const renderTask = (taskListElement, task) => {
-//   const replaceTaskToEdit = () => {
-//     taskListElement.replaceChild(taskEditComponent.getElement(), taskComponent.getElement());
-//   };
-
-
-//   const replaceEditToTask = () => {
-//     taskListElement.replaceChild(taskComponent.getElement(), taskEditComponent.getElement());
-//   };
-
-//   const onEscKeyDown = (evt) => {
-//     const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
-
-//     if (isEscKey) {
-//       replaceEditToTask();
-//       document.removeEventListener(`keydown`, onEscKeyDown);
-//     }
-//   };
-
-//   const taskComponent = new TaskComponent(task);
-//   const editButton = taskComponent.getElement().querySelector(`.card__btn--edit`);
-//   editButton.addEventListener(`click`, () => {
-//     replaceTaskToEdit();
-//     document.addEventListener(`keydown`, onEscKeyDown);
-//   });
-
-//   const taskEditComponent = new TaskEditComponent(task);
-//   const editForm = taskEditComponent.getElement().querySelector(`form`);
-//   editForm.addEventListener(`submit`, (evt) => {
-//     evt.preventDefault();
-//     replaceEditToTask();
-//     document.removeEventListener(`keydown`, onEscKeyDown);
-//   });
-
-//   render(taskListElement, taskComponent.getElement(), RenderPosition.BEFOREEND);
-// };
-
 const renderBoard = (boardComponent, tasks) => {
   const isAllTasksArchived = tasks.every((task) => task.isArchive);
 
